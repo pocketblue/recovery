e94f2ca6e4564e231169e34cb0c9b1351f3e96c9
gmanka <gmankab@gmail.com>
2025-10-07 22:32:04 +0500

    [+] nothing-pong

---
+++ nothing-pong/pitchblack.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -xo pipefail
+
+export ALLOW_MISSING_DEPENDENCIES=true
+
+# sync
+mkdir build
+cd build
+git config --global color.ui auto
+repo init -u https://github.com/PitchBlackRecoveryProject/manifest_pb -b android-12.1
+repo sync
+git clone https://github.com/PitchBlackRecoveryProject/android_device_nothing_Pong-pbrp -b android-12.1 device/nothing/Pong
+
+# extract dtb
+curl -LO https://mirrorbits.lineageos.org/full/Pong/20251001/vendor_boot.img
+python system/tools/mkbootimg/unpack_bootimg.py --boot_img=vendor_boot.img --out=vendor_boot_unpacked
+mkdir device/nothing/Pong/prebuilt/dtbs
+cp vendor_boot_unpacked/dtb device/nothing/Pong/prebuilt/dtbs/pong.dtb
+grep BOARD_INCLUDE_DTB_IN_BOOTIMG device/nothing/Pong/BoardConfigCommon.mk || \
+   cat ../../BoardConfigAppend.mk >> device/nothing/Pong/BoardConfigCommon.mk
+
+# build
+source build/envsetup.sh
+lunch pb_Pong-eng
+mka vendorbootimage
+
+cp out/target/product/Pong/vendor_boot.img $GITHUB_WORKSPACE/pitchblack-nothing-pong.img
---
+++ nothing-pong/twrp.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -xo pipefail
+
+export ALLOW_MISSING_DEPENDENCIES=true
+
+# sync
+mkdir build
+cd build
+git config --global color.ui auto
+repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp -b twrp-12.1
+repo sync
+git clone https://gitlab.com/OrangeFox/device/Pong -b fox_12.1 device/nothing/Pong
+
+# extract dtb
+curl -LO https://mirrorbits.lineageos.org/full/Pong/20251001/vendor_boot.img
+python system/tools/mkbootimg/unpack_bootimg.py --boot_img=vendor_boot.img --out=vendor_boot_unpacked
+mkdir device/nothing/Pong/prebuilt/dtbs
+cp vendor_boot_unpacked/dtb device/nothing/Pong/prebuilt/dtbs/pong.dtb
+grep BOARD_INCLUDE_DTB_IN_BOOTIMG device/nothing/Pong/BoardConfigCommon.mk || \
+   cat ../../BoardConfigAppend.mk >> device/nothing/Pong/BoardConfigCommon.mk
+
+# build
+source build/envsetup.sh
+lunch twrp_Pong-eng
+mka vendorbootimage
+
+cp out/target/product/Pong/vendor_boot.img $GITHUB_WORKSPACE/twrp-nothing-pong.img
